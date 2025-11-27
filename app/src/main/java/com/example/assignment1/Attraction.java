9dc6d304554513fb5aa13de9065feaf9acb8d25f
Emancsc <alghalbaneman@gmai.com>
2025-11-27 19:56:53 +0200

    layouts

--- app/src/main/java/com/example/assignment1/Attraction.java
+++ app/src/main/java/com/example/assignment1/Attraction.java
@@ -1,4 +1,112 @@
 package com.example.assignment1;
 
+import java.util.UUID;
+
 public class Attraction {
+    private String attractionId;
+    private String name;
+    private String location;
+    private String type;
+    private int rating; //1-5
+    private double entryFee;
+    private String visitDate; //yyyy-mm-dd
+    private String duration; //in housr
+    private boolean isVisited;
+    private boolean isFavorite;
+    private String notes;
+
+    public Attraction() {
+        this.attractionId = UUID.randomUUID().toString();
+        this.isVisited = false;
+        this.isFavorite = false;
+        this.rating = 1;
+    }
+
+    public String getAttractionId() {
+        return attractionId;
+    }
+
+    public void setAttractionId(String attractionId) {
+        this.attractionId = attractionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    public boolean isVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getVisitDate() {
+        return visitDate;
+    }
+
+    public void setVisitDate(String visitDate) {
+        this.visitDate = visitDate;
+    }
+
+    public double getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(double entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
